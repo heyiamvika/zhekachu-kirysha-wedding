7b767cf958dc16491a4531a77c77ae7a5310b852
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-04-11 22:49:23 +0300

    Added images to locations page

--- src/app/(ui)/[slug]/LocationPage.tsx
+++ src/app/(ui)/[slug]/LocationPage.tsx
@@ -1,6 +1,33 @@
-const LocationPage = () => (
-  // TODO: обрізається картинка
-  <div className='bg-[url(/location.png)] w-full h-full bg-center bg-cover bg-no-repeat flex justify-end items-start pt-2' />
-);
+import Image from 'next/image';
+
+const LocationPage = () => {
+  // TODO: Fix background
+  // TODO: Fix font
+  // TODO: Fix locations
+  return (
+    <div className='bg-[url(/location.png)] w-full h-screen bg-center bg-cover bg-no-repeat flex p-6'>
+      <button className='w-12.5 h-11'>
+        <Image
+          src='/right-arrow.png'
+          alt='стрілка вправо'
+          width={50}
+          height={14}
+        />
+      </button>
+      <span className=''>Локація</span>
+      <div>
+        <Image
+          src='/arrow-down.png'
+          alt='стрілка вниз'
+          width={85}
+          height={82}
+        />
+      </div>
+      <div className='self-end'>
+        <span className=''>с. Раковець forest event</span>
+      </div>
+    </div>
+  );
+};
 
 export default LocationPage;
