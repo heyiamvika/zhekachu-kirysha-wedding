9af4f85fef6d449bb5585100039d7c2ccf5b2066
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-04-11 22:26:49 +0300

    Added location page

--- src/app/(ui)/[slug]/LocationPage.tsx
+++ src/app/(ui)/[slug]/LocationPage.tsx
@@ -1,18 +1,6 @@
-import Image from 'next/image';
-
-const LocationPage = () => {
-  return (
-    <>
-      <button>
-        <Image
-          src='/right-arrow.png'
-          alt='стрілка вправо'
-          width={44}
-          height={16}
-        />
-      </button>
-    </>
-  );
-};
+const LocationPage = () => (
+  // TODO: обрізається картинка
+  <div className='bg-[url(/location.png)] w-full h-full bg-center bg-cover bg-no-repeat flex justify-end items-start pt-2' />
+);
 
 export default LocationPage;
