be1a511b9f2ad94667757aca6a6acc0bb1ebc1a4
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-04-11 19:41:19 +0300

    Added placeholder data

---
+++ src/app/(lib)/definitions.ts
@@ -0,0 +1,4 @@
+export type Guest = {
+  slug: string;
+  name: string;
+};
---
+++ src/app/(lib)/placeholder-data.ts
@@ -0,0 +1,20 @@
+import { Guest } from './definitions';
+
+export const guests: Guest[] = [
+  {
+    slug: 'zhenya',
+    name: 'Женя',
+  },
+  {
+    slug: 'vika',
+    name: 'Віка',
+  },
+  {
+    slug: 'marina',
+    name: 'Марина',
+  },
+  {
+    slug: 'oleh',
+    name: 'Олег',
+  },
+];
