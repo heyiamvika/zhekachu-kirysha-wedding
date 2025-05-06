40cf3109a98ca9ccff8917f77b31801aa0795537
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-05-06 14:27:13 +0300

    Start page layout

--- app/ui/StartPage.tsx
+++ app/ui/StartPage.tsx
@@ -1,6 +1,21 @@
+import Image from 'next/image';
+
 const StartPage = () => {
-  // TODO: add layout
-  return <div>Start</div>;
+  return (
+    <div className='h-full w-full flex items-end justify-center relative'>
+      <Image
+        src='/parcel-open.png'
+        width={375}
+        height={400}
+        className='w-full absolute top-0 max-h-screen lg:w-[50%] '
+        alt='Клапан конверта'
+      />
+      <span className='text-big z-10 mb-[25vh] sm:max-w-94 text-center'>
+        {/* TODO: Add name */}
+        Привіт, Віка!
+      </span>
+    </div>
+  );
 };
 
 export default StartPage;
