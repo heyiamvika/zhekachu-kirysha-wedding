94e9e9a3592352d3cea51a318f8515f217ac2211
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-05-06 16:50:50 +0300

    Added main page animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,39 @@
 'use client';
 
-import { MouseEventHandler, useState } from 'react';
+import { useState } from 'react';
 import { Step } from '@/app/lib/definitions';
 import { PAGES } from '@/app/lib/pages';
 import { renderStep } from '@/app/ui/renderStep';
 
 export default function Home() {
-  const [currentStep, setCurrentStep] = useState<Step>(PAGES.START);
+  const [currentStep] = useState<Step>(PAGES.START);
 
-  const nextStep = () => {
-    setCurrentStep((prevStep) => prevStep + 1);
-  };
+  // const nextStep = () => {
+  //   setCurrentStep((prevStep) => prevStep + 1);
+  // };
 
-  const prevStep = () => {
-    if (currentStep === PAGES.START || currentStep === PAGES.LOCATION_STEP)
-      return;
+  // const prevStep = () => {
+  //   if (currentStep === PAGES.START || currentStep === PAGES.LOCATION_STEP)
+  //     return;
 
-    setCurrentStep((prevStep) => prevStep - 1);
-  };
+  //   setCurrentStep((prevStep) => prevStep - 1);
+  // };
 
-  const handleScreenClick: MouseEventHandler = (event) => {
-    const clickX = event.clientX;
-    const screenWidth = window.innerWidth;
+  // const handleScreenClick: MouseEventHandler = (event) => {
+  //   const clickX = event.clientX;
+  //   const screenWidth = window.innerWidth;
 
-    if (clickX < screenWidth / 2) {
-      prevStep();
-    } else {
-      nextStep();
-    }
-  };
+  //   if (clickX < screenWidth / 2) {
+  //     prevStep();
+  //   } else {
+  //     nextStep();
+  //   }
+  // };
 
   return (
     <div
       className='w-full h-screen flex justify-center items-center'
-      onClick={handleScreenClick}
+      // onClick={handleScreenClick}
     >
       {renderStep(currentStep)}
     </div>
