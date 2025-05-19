a1bd677758d5acbbd56b2fed6ceb7130355c45c6
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-05-19 17:46:23 +0300

    Added NotFoundPage

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import { guests } from '../lib/placeholder-data';
+import GuestPage from '../ui/pages/GuestPage';
+import NotFoundPage from '../ui/pages/NotFoundPage';
+
+export async function generateStaticParams() {
+  // TODO: Change to data from Google Sheets
+  const guestsPages = guests;
+
+  console.log({ guestsPages });
+
+  return guestsPages.map((page) => ({
+    slug: page.slug,
+  }));
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const guest = guests.find((guest) => guest.slug === slug);
+
+  console.log({ slug, guest });
+
+  return guest ? <GuestPage /> : <NotFoundPage />;
+}
---
+++ app/ui/pages/GuestPage.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import { MouseEventHandler, useState } from 'react';
+import { FormData, Step } from '@/app/lib/definitions';
+import { PAGES } from '@/app/lib/pages';
+import { renderStep } from '@/app/ui/helpers';
+import DesktopPage from './DesktopPage';
+
+const MOBILE_MAX_WIDTH = 430;
+
+const initialFormData: FormData = {
+  transportation: null,
+  hotel: null,
+  allergies: null,
+  alcohol: null,
+};
+
+export default function GuestPage() {
+  const [formData, setFormData] = useState(initialFormData);
+  const [currentStep, setCurrentStep] = useState<Step>(PAGES.START);
+
+  const handleFormValueSet = (key: keyof FormData, value: string) => {
+    setFormData((prevState) => ({ ...prevState, [key]: value }));
+    handleNextStep();
+  };
+
+  const handleSubmitForm = () => {
+    console.log({ formData });
+  };
+
+  const handleNextStep = () => {
+    if (currentStep === PAGES.CONFIRMATION_STEP) return;
+
+    setCurrentStep((prevStep) => prevStep + 1);
+  };
+
+  const handlePrevStep = () => {
+    if (currentStep === PAGES.START || currentStep === PAGES.LOCATION_STEP)
+      return;
+
+    setCurrentStep((prevStep) => prevStep - 1);
+  };
+
+  const handleScreenClick: MouseEventHandler = (event) => {
+    if (currentStep === PAGES.START) return;
+
+    const clickX = event.clientX;
+    const screenWidth = window.innerWidth;
+
+    if (clickX < screenWidth / 2) {
+      handlePrevStep();
+    } else {
+      handleNextStep();
+    }
+  };
+
+  if (window.innerWidth > MOBILE_MAX_WIDTH) {
+    return <DesktopPage />;
+  }
+
+  return (
+    <div onClick={handleScreenClick} className='@container'>
+      {renderStep(currentStep, {
+        onNext: handleNextStep,
+        onPrev: handlePrevStep,
+        onFormValueSet: handleFormValueSet,
+        onFormSubmit: handleSubmitForm,
+      })}
+    </div>
+  );
+}
