a165cce3d6f70b7749ce6dd9668574df890ed736
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-05-26 17:11:43 +0300

    Fixed confirmed page UI

--- app/ui/pages/ConfirmedPage.tsx
+++ app/ui/pages/ConfirmedPage.tsx
@@ -2,9 +2,17 @@ import Image from 'next/image';
 
 export const ConfirmedPage = () => {
   return (
-    <div className='h-screen pt-10 pb-15 px-4 flex justify-center items-center'>
-      <div className='flex flex-col items-center gap-15'>
-        <Image src='/fireworks.png' alt='Феєрверки' width={164} height={105} />
+    <div className='h-screen pt-10 pb-15 flex flex-col'>
+      <div className='px-19.5'>
+        <Image
+          src='/fireworks.png'
+          alt='Феєрверки'
+          width={164}
+          height={105}
+          className='w-full h-full'
+        />
+      </div>
+      <div className='grow px-4 flex flex-col items-center justify-center gap-6 min-w-[288] max-w-[480]'>
         <span className='text-center'>Єєє!</span>
         <span className='text-center'>
           Тепер весілля офіційно на 100% круте 🎉 Чекаємо з нетерпінням!
