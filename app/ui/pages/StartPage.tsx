8d11faf23cecb3826fe999c6cd59428e2ecafff0
Viktoriia Nosova <v.nosova@steel.mil.ua>
2025-05-27 22:43:37 +0300

    Fixed start page

--- app/ui/pages/StartPage.tsx
+++ app/ui/pages/StartPage.tsx
@@ -1,99 +1,104 @@
 'use client';
 
-// import clsx from 'clsx';
-// import { MouseEventHandler, useState } from 'react';
-// import { useWindowWidth } from '../../lib/hooks';
-// import { useAppStore } from '@/app/lib/hooks';
+import clsx from 'clsx';
+import Image from 'next/image';
+import { MouseEventHandler, useState } from 'react';
+import { useAppStore, useWindowWidth } from '@/app/lib/hooks';
 
 export const StartPage = () => {
-  // const [isParcelOpen, setIsParcelOpen] = useState(false);
+  const [isParcelOpen, setIsParcelOpen] = useState(false);
 
-  // const windowWidth = useWindowWidth();
-  // const { onNextStep, guest } = useAppStore((state) => state);
+  const windowWidth = useWindowWidth();
+  const { onNextStep, guest } = useAppStore((state) => state);
 
-  // const handleScreenClick: MouseEventHandler = ({ clientX }) => {
-  //   if (!windowWidth) return;
-  //   const clickedRightHalf = clientX >= windowWidth / 2;
+  const handleScreenClick: MouseEventHandler = ({ clientX }) => {
+    if (!windowWidth) return;
+    const clickedRightHalf = clientX >= windowWidth / 2;
 
-  //   if (clickedRightHalf) {
-  //     onNextStep();
-  //   }
-  // };
+    if (clickedRightHalf) {
+      onNextStep();
+    }
+  };
 
   return (
-    <div className='h-dvh w-full bg-[url(/main-photo.jpg)] bg-cover bg-top'>
-      {/* <OpenBackground
+    <div className='h-dvh w-full bg-[url(/main-photo.jpg)] bg-cover bg-top overflow-hidden relative'>
+      <OpenBackground
         isParcelOpen={isParcelOpen}
         onScreenClick={handleScreenClick}
-      /> */}
-      {/* <ClosedParcel
+      />
+      <ClosedParcel
         guestName={guest!.name}
         isParcelOpen={isParcelOpen}
         onOpen={() => setIsParcelOpen(true)}
-      /> */}
+      />
     </div>
   );
 };
 
-// type ClosedParcelProps = {
-//   guestName: string;
-//   isParcelOpen: boolean;
-//   onOpen: () => void;
-// };
+type ClosedParcelProps = {
+  guestName: string;
+  isParcelOpen: boolean;
+  onOpen: () => void;
+};
 
-// const ClosedParcel = ({
-//   guestName,
-//   isParcelOpen,
-//   onOpen,
-// }: ClosedParcelProps) => {
-//   const animationStyles = 'transition ease-out transform duration-4000';
+const ClosedParcel = ({
+  guestName,
+  isParcelOpen,
+  onOpen,
+}: ClosedParcelProps) => {
+  const animationStyles = 'transition ease-out transform duration-4000';
 
-//   return (
-//     <>
-//       <div
-//         onClick={onOpen}
-//         className={clsx(
-//           'bg-[url(/envelope-top.svg)] drop-shadow-2xl bg-cover bg-center w-full h-115 max-h-[60%] absolute top-0 z-10',
-//           {
-//             [`${animationStyles} -translate-y-[100%]`]: isParcelOpen,
-//           }
-//         )}
-//       />
-//       <div
-//         className={clsx(
-//           'bg-[url(/envelope-bottom.png)] bg-cover bg-center absolute bottom-0 w-full flex items-end justify-center',
-//           {
-//             [`${animationStyles} translate-y-[100%]`]: isParcelOpen,
-//           }
-//         )}
-//       >
-//         <span className='text-big mb-[15vh] sm:mb-[25vh] max-w-[80%] text-center'>
-//           {`Привіт, ${guestName}!`}
-//         </span>
-//       </div>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <div
+        onClick={onOpen}
+        className={clsx('drop-shadow-2xl w-full absolute top-0 z-10', {
+          [`${animationStyles} -translate-y-full`]: isParcelOpen,
+        })}
+      >
+        <Image
+          src='/envelope-top.svg'
+          alt='Клапан конверта'
+          width={375}
+          height={399}
+          className='w-full object-cover'
+        />
+      </div>
+      <div
+        className={clsx(
+          'absolute bottom-0 bg-[url(/envelope-bottom.png)]  w-full h-full bg-cover bg-center flex items-end justify-center',
+          {
+            [`${animationStyles} translate-y-full`]: isParcelOpen,
+          }
+        )}
+      >
+        <span className='text-big mb-[15vh] sm:mb-[25vh] max-w-[80%] text-center'>
+          {`Привіт, ${guestName}!`}
+        </span>
+      </div>
+    </>
+  );
+};
 
-// type OpenBackgroundProps = {
-//   isParcelOpen: boolean;
-//   onScreenClick: MouseEventHandler;
-// };
+type OpenBackgroundProps = {
+  isParcelOpen: boolean;
+  onScreenClick: MouseEventHandler;
+};
 
-// const OpenBackground = ({
-//   isParcelOpen,
-//   onScreenClick,
-// }: OpenBackgroundProps) => {
-//   return (
-//     <div
-//       className={clsx(
-//         'absolute top-0 w-full text-white text-big flex flex-col items-center text-center py-10 sm:py-20 justify-between bg-black/20',
-//         { 'animate-fade-in': isParcelOpen }
-//       )}
-//       onClick={onScreenClick}
-//     >
-//       <span>Запрошуємо</span>
-//       <span className='mb-15'>тебе на наше весілля 31 липня!</span>
-//     </div>
-//   );
-// };
+const OpenBackground = ({
+  isParcelOpen,
+  onScreenClick,
+}: OpenBackgroundProps) => {
+  return (
+    <div
+      className={clsx(
+        'absolute top-0 w-full h-full text-white text-big flex flex-col items-center text-center py-10 sm:py-20 justify-between bg-black/20',
+        { 'animate-fade-in': isParcelOpen }
+      )}
+      onClick={onScreenClick}
+    >
+      <span>Запрошуємо</span>
+      <span className='mb-15'>тебе на наше весілля 31 липня!</span>
+    </div>
+  );
+};
